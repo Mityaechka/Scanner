4edff3ec66c46ae3e2bbf40d5f450e65a87a28b1
Mityaechka <50111604+Mityaechka@users.noreply.github.com>
2020-05-24 22:36:20 +0600

    SendBarcode function

--- js/chrome.js
+++ js/chrome.js
@@ -19,14 +19,29 @@ function LoadSettings(onload) {
 function SaveSettings(settings) {
   chrome.storage.sync.set({ settings: JSON.stringify(settings) });
 }
+/**
+ *
+ * @param {string} barcode
+ */
 function SendBarcode(barcode) {
+  if (!barcode.match(/^[0-9]{13,20}/)) {
+    console.error(`${barcode} has incorrect format`);
+    return;
+  }
   LoadSettings(function (settings) {
-    if (settings.callFunction && settings.function != undefined)
-      location.href = `javascript:${settings.function}('${barcode}'); void 0`;
-
+    if (settings.callFunction && settings.function != undefined) {
+      try {
+        location.href = `javascript:${settings.function}('${barcode}'); void 0`;
+      } catch (e) {
+        console.error(`function ${settings.function} not found`);
+      }
+    }
     if (settings.embedHtml) {
       var element = document.getElementById(settings.id);
-      if (element == null) return;
+      if (element == null) {
+        console.error(`element ${settings.id} not found`);
+        return;
+      }
       if (settings.embedText) element.innerText = barcode;
       else if (settings.attribute != "") {
         element.removeAttribute(settings.attribute);
