4a05e5f3e931368ea65acf53b11e4e639651f8c0
pavlov32sh1 <pavlov32sh1@gmail.com>
2020-05-25 11:39:31 +0600

    Add lib

--- js/scanner.js
+++ js/scanner.js
@@ -2,35 +2,11 @@ let barcode = "";
 let prevTime = Date.now();
 let firstInput = true;
 
-document.onkeydown = function (e) {
-  let time = Date.now();
+onScan.attachTo(document);
 
-  var delta = time - prevTime;
-
-  prevTime = time;
-  let code = e.keyCode ? e.keyCode : e.which;
-  console.log(String.fromCharCode(code));
-  if (!firstInput) {
-    if (delta > 20) {
-      barcode = "";
-    }
-  } else {
-    firstInput = false;
-  }
-
-  if (code == 13 || code == 9) {
-    if (barcode != "") {
-      SendBarcode(barcode);
-      barcode = "";
-    }
-    firstInput = true;
-  } else {
-    let key =String.fromCharCode(code);
-    if (key.match(/^[0-9a-zA-Z]/))
-    barcode = barcode + key;
-  }
-  
-};
+document.addEventListener('scan', function(scancode, iQuantity) {
+    SendBarcode(scancode.detail.scanCode);
+});
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.barcode) SendBarcode(request.barcode);
